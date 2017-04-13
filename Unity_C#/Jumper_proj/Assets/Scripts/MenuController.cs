e9b05871a052b0f2d6482a0a20382681fc5a04f9
Volodymyr <docentkafedru@gmail.com>
2017-04-14 00:11:45 +0300

    jumper

---
+++ Unity_C#/Jumper_proj/Assets/Scripts/MenuController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject pannel;
+    public GameObject start;
+    public Text ControlKeys;
+    public Text SoundTxt;
+
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Option()
+    {
+        pannel.SetActive(true);
+        start.SetActive(false);
+    }
+    public void OptionOK()
+    {
+        pannel.SetActive(false);
+        start.SetActive(true);
+    }
+    public void Keys()
+    {
+        if (PlayerPrefs.GetString("Keys") != "off")
+        {
+            PlayerPrefs.SetString("Keys", "off");
+            ControlKeys.text = "Control mode: acceleration";
+        }
+        else
+        {
+            PlayerPrefs.SetString("Keys", "on");
+            ControlKeys.text = "Control mode: Control Keys";
+        }
+        
+    }
+    public void Sound()
+    {
+        if (PlayerPrefs.GetString("Sound") != "off")
+        {
+            PlayerPrefs.SetString("Sound", "off");
+            SoundTxt.text = "Sound: off";
+        }
+        else
+        {
+            PlayerPrefs.SetString("Sound", "yes");
+            SoundTxt.text = "Sound: on";
+        }
+    }
+
+}
